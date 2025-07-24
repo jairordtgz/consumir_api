61af6dd2f0c8d260a6ca5a60b271d01b7662bda5
jairordtgz <jairodri.a05@gmail.com>
2025-07-23 23:45:08 -0500

    segundo commit con routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './assets/css/App.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import Login from './components/Login';
+import {BrowserRouter as Router ,Routes,Route} from 'react-router-dom'
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Router>
+        <Routes> 
+          <Route path='/' element={<Login/>}/>
+          <Route path='/dashboard' element={<Dashboard/>}/> {/*ruta y renderizar archivo*/} 
+        </Routes>
+
+      </Router>
+
+    </React.Fragment>
   );
 }
 
